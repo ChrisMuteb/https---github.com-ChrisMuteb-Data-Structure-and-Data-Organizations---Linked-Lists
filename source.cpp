9ac5d26f9b69de460ad4637fac091ad8ece25b2a
ChrisMuteb <ibond.chris@gmail.com>
2022-12-22 12:43:04 +0200

    creation of Linked List with two nodes - function

--- source.cpp
+++ source.cpp
@@ -7,17 +7,33 @@ struct node{
     struct node *next;
 };
 
+struct node* createNode(int );
+
 int main(){
 
     struct node *header = NULL;
-    header = (struct node *)malloc(sizeof(node));
-    header->data = 52;
-    header->next = NULL;
+    header = createNode(52);
+    header->next = createNode(48); 
 
     cout << header << endl;
     cout << header->data << endl;
     cout << header->next << endl;
+    cout << header->next->data << endl;
+    cout << header->next->next << endl;
 
     system("PAUSE");
     return 0;
+}
+
+struct node* createNode(int item){
+    //1. create a temp node
+    struct node *temp;
+    //2. allocate memory for node
+    temp = (struct node *)malloc(sizeof(node));
+    //3. assign item to node
+    temp->data = item;
+    //4. assign null to next node.
+    temp->next = NULL;
+    //5. return newly created node.
+    return temp;
 }
