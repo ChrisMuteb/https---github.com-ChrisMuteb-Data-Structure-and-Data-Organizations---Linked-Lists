64f8cba37136f4aaf2c03f71af3b41c9b2639824
ChrisMuteb <ibond.chris@gmail.com>
2022-12-22 12:30:55 +0200

    Creation of Linked List

--- source.cpp
+++ source.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+struct node{
+    int data;
+    struct node *next;
+};
+
 int main(){
 
-    // cout << "Hello world";
+    struct node *header = NULL;
+    header = (struct node *)malloc(sizeof(node));
+    header->data = 52;
+    header->next = NULL;
 
+    cout << header << endl;
+    cout << header->data << endl;
+    cout << header->next << endl;
+
+    system("PAUSE");
     return 0;
 }
